4bb5b931f3449446d9b75992e922ef0a7355e31f
shimizu.kenji <shimizu.kenji@noncore.net>
2021-01-31 01:08:14 +0900

    モブタイマーページを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
-import logo from './logo.svg'
-import './App.css'
+import { Box, Button, Container, makeStyles } from '@material-ui/core'
+import { KeyboardOutlined } from '@material-ui/icons'
+
+const useStyles = makeStyles({
+  status: {
+    fontSize: 64,
+  },
+  driverIcon: {
+    fontSize: 64,
+  },
+  driver: {
+    fontSize: 64,
+  },
+  remainingTime: {
+    marginTop: 32,
+    fontSize: 64,
+  },
+  actions: {
+    '& > *': {
+      margin: 8,
+    },
+  },
+})
 
 function App() {
+  const classes = useStyles()
+  const driver = 'Typist Name'
+  const remainingTime = 365
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Box display={'flex'} flexDirection={'column'} alignItems={'center'}>
+        <div className={classes.status}>1st Interval</div>
+        <Box display={'flex'} alignItems={'center'}>
+          <KeyboardOutlined className={classes.driverIcon} />
+          <div className={classes.driver}>{driver}</div>
+        </Box>
+        <Box display={'flex'} alignItems={'center'}>
+          <div className={classes.remainingTime}>
+            {Math.floor(remainingTime / 60)} min {remainingTime % 60} sec
+          </div>
+        </Box>
+        <Box display={'flex'} alignItems={'center'} className={classes.actions}>
+          <Button variant={'contained'} color={'primary'}>
+            Member Settings
+          </Button>
+          <Button variant={'contained'} color={'primary'}>
+            Interval Settings
+          </Button>
+        </Box>
+      </Box>
+    </Container>
   )
 }
 
